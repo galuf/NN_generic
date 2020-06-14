504eb398362244d78af3b435e6e0ae6437b02ee7
galuf <alex.flores.m@uni.pe>
2020-06-14 09:48:30 -0500

    red neuronal 98%

--- src/nn_generic/Main.java
+++ src/nn_generic/Main.java
@@ -3,33 +3,21 @@ package nn_generic;
 public class Main {
     
     public static void main(String[] args) {
-
-    double [][] input = {{1},{1}};
-    double [][] output = {{1}};
-
-    double [][] pesos1 = {{2,-2},{1,3}};
-    double [][] pesos2 = {{3,-2}};
-    Rn01.mostrar(Rn01.onesRow(pesos1));
-    System.out.println();
-    Rn01.mostrar(Rn01.onesColumn(input));
-    System.out.println();
-    Rn01.mostrar(Rn01.dot(Rn01.onesRow(pesos1),Rn01.onesColumn(input)));
-    System.out.println();
-    Rn01.mostrar(Rn01.matrizSigmoidea(Rn01.dot(Rn01.onesRow(pesos1),Rn01.onesColumn(input))));
-    System.out.println();
-    Rn01.mostrar(Rn01.onesColumn(Rn01.matrizSigmoidea(Rn01.dot(Rn01.onesRow(pesos1),Rn01.onesColumn(input)))));
-    System.out.println();
-    Rn01.mostrar(Rn01.dot(Rn01.onesRow(pesos2),Rn01.onesColumn(Rn01.matrizSigmoidea(Rn01.dot(Rn01.onesRow(pesos1),Rn01.onesColumn(input))))));
-    System.out.println();
-    Rn01.mostrar(Rn01.matrizSigmoidea(Rn01.dot(Rn01.onesRow(pesos2),Rn01.onesColumn(Rn01.matrizSigmoidea(Rn01.dot(Rn01.onesRow(pesos1),Rn01.onesColumn(input)))))));
-    
-    Rn01.example_varargs(1,3,4);
-    Rn01.example_varargs(1,4,5,6,7);
-    // mostrar(onesColumn(m2));
-    // mostrar(onesRow(m1));
-    // mostrar(transpuesta(m2));
-    // mostrar(dot(m1,m2));
-    // mostrar(matrizSigmoidea(dot(m1,m2)));
-  }
-    
+        
+        double ingreso[][]={
+                            {1,1,1,1,0,1,1,1,1,1,0,1,1,0,1},
+                            {1,1,1,1,0,0,1,1,1,1,0,0,1,1,1},
+                            {0,1,0,0,1,0,0,1,0,0,1,0,0,1,0},
+                            {1,1,1,1,0,1,1,0,1,1,0,1,1,1,1}};
+        double salida[][]={{0,0},{0,1},{1,0},{1,1}};
+        double evaluar[][]={
+                            {1,1,0,1,0,1,1,1,1,1,0,1,0,0,1}, //salida debe ser : {0,0}
+                            {1,1,1,1,0,0,1,1,0,1,0,0,1,1,1}, //salida debe ser : {0,1}
+                            {0,1,1,0,1,0,0,1,0,0,1,0,1,1,0}, //salida debe ser : {1,0}
+                            {1,1,0,1,0,1,1,0,1,1,0,1,1,1,0}};//salida debe ser : {1,1} 
+        //RnGen01 rn = new RnGen01(15,15,15,3,3,3,3,3,2); // (ingreso , capa oculta, salidas)
+        RnGen01 rn = new RnGen01(15,15,3,2);
+        rn.entrenamiento(ingreso, salida,1000);
+        rn.prueba(evaluar);
+    }   
 }
